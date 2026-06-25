0af7fc0bb146a5ec75aafbc13c8aec565a6eb24f
jacintandungu-cell <jacinta.ndungu@student.moringaschool.com>
2026-06-25 13:39:23 +0300

    Initial Commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,22 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        """
+        Send an HTTP GET request to the stored URL
+        and return the raw response body as text.
+        """
+        response = requests.get(self.url)
+        response.raise_for_status()  # raises error if request fails
+        return response.text
 
     def load_json(self):
-        pass
+        """
+        Call get_response_body, then parse the response
+        into Python data structures using json.loads.
+        """
+        response_body = self.get_response_body()
+        return json.loads(response_body)
